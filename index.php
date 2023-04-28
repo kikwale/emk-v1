da4e05b7640a1ceaf669a782faa2e1f368ee812b
expertfilly <msuyafilbert@gmail.com>
2023-04-28 16:49:46 +0300

    new updates

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include('partials/about.php');
 include('partials/bestcompany.php');
 include('partials/services.php');
 include('partials/consultancy.php');
-include('partials/testimonial.php');
+// include('partials/testimonial.php');
 include('partials/portifolio.php');
 // include('partials/posts.php');
 include('partials/footer.php');
